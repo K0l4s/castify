6d0302a0764f9876cb92f9a1c2e30b32c09f0fe8
Trung Kien <21110223@student.hcmute.edu.vn>
2024-10-06 02:13:29 +0700

    deploy: fix error

--- views/src/main.tsx
+++ views/src/main.tsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <Router >
         <App />
-      </BrowserRouter>
+      </Router >
     </Provider>
   </StrictMode>,
 )
