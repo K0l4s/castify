4c39fe78f6b55c83002258929ad099ddcd9a5856
Trung Kien <21110223@student.hcmute.edu.vn>
2024-11-05 14:37:17 +0700

    theme config

--- views/src/App.tsx
+++ views/src/App.tsx
@@ -1,13 +1,16 @@
 // import { useState } from 'react'
 
 import './App.css'
+import { ThemeProvider } from './context/ThemeContext'
 import Router from './routers/Router'
 
 function App() {
 
   return (
     <>
+      <ThemeProvider>
         <Router />
+      </ThemeProvider>
     </>
   )
 }
