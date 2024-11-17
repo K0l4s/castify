fdb09ff30b9cc5cee9201704af8072cf216c376c
Trung Kien <21110223@student.hcmute.edu.vn>
2024-11-17 16:15:18 +0700

    .

--- views/vite.config.ts
+++ views/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // Mở server để truy cập từ các thiết bị trong cùng mạng LAN
+    // host: true, // Mở server để truy cập từ các thiết bị trong cùng mạng LAN
     port: 3000, // Thay đổi cổng nếu cần thiết
   },
 })
