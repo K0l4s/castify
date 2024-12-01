27198d538274d2df104d1f26c6526ba0956cb2c5
Trung Kien <21110223@student.hcmute.edu.vn>
2024-12-01 07:29:25 +0700

    fix deploy

--- views/src/services/AuthenticateService.ts
+++ views/src/services/AuthenticateService.ts
@@ -1,7 +1,7 @@
 // import axios from "axios";
 
 import { LoginInput } from "../models/Authentication";
-import { userRegister } from "../models/User";
+// import { userRegister } from "../models/User";
 // import Cookies from 'js-cookie';
 import { axiosInstance } from "../utils/axiosInstance";
 
