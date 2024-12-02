72e4b7064dbf7de43ef322fbad4186afea2d390d
nauht1 <thuanngo454@gmail.com>
2024-12-02 22:09:43 +0700

    modify (views): change podcast tag hover

--- views/src/components/UI/podcast/PodcastTag.tsx
+++ views/src/components/UI/podcast/PodcastTag.tsx
@@ -3,6 +3,7 @@ import { Podcast } from '../../../models/PodcastModel';
 import { formatDistanceToNow } from 'date-fns';
 import default_avatar from '../../../assets/images/default_avatar.jpg';
 import { Link } from 'react-router-dom';
+import { FaPlay } from 'react-icons/fa';
 
 interface PodcastTagProps {
   podcast: Podcast;
@@ -13,13 +14,18 @@ const PodcastTag: React.FC<PodcastTagProps> = ({ podcast }) => {
   const createdDay = formatDistanceToNow(new Date(podcast.createdDay), { addSuffix: true });
 
   return (
-    <div className="rounded-lg overflow-hidden transform transition-transform duration-300">
-      <Link to={`/watch?pid=${podcast.id}`}>
-        <img src={podcast.thumbnailUrl || "/TEST.png"} alt={podcast.title} className="w-full h-56 object-fit" />
+    <div className="relative rounded-lg overflow-hidden transform transition-transform duration-300">
+      <Link to={`/watch?pid=${podcast.id}`} className="block">
+        <div className="relative group">
+          <img src={podcast.thumbnailUrl || "/TEST.png"} alt={podcast.title} className="w-full h-56 object-fit" />
+          <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <FaPlay className="text-white text-4xl" />
+          </div>
+        </div>
       </Link>
       <div className="p-2 flex">
         <Link to={`/profile/${podcast.user.username}`}>
-          <img src={podcast.user.avatarUrl || default_avatar} alt={author} className="w-10 h-10 object-cover rounded-full mr-4" />
+          <img src={podcast.user.avatarUrl || default_avatar} alt={author} className="w-10 h-10 object-fit rounded-full mr-4" />
         </Link>
         <div className="flex flex-col justify-between">
           <div>
