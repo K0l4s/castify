a0673f49690673cac2b5ad7d10daa7831fb518b1
Trung Kien <21110223@student.hcmute.edu.vn>
2024-12-03 11:43:50 +0700

    user admin (api)

---
+++ backend/src/main/java/com/castify/backend/models/dashboard/OverviewModel.java
@@ -0,0 +1,23 @@
+package com.castify.backend.models.dashboard;
+
+import com.castify.backend.models.podcast.PodcastModel;
+import com.castify.backend.models.user.BasicUserModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DashboardModel {
+    private List<BasicUserModel> newUsers = new ArrayList<>();
+    private List<BasicUserModel> activeUsers = new ArrayList<>();
+    private List<PodcastModel> newPodcasts = new ArrayList<>();
+    private long totalLikes;
+    private long totalComments;
+    private long totalViews;
+    
+}
---
+++ backend/src/main/java/com/castify/backend/models/paginated/PaginatedResponse.java
@@ -0,0 +1,15 @@
+package com.castify.backend.models.paginated;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaginatedResponse<T> {
+    private List<T> items;
+    private int totalPages;
+}
