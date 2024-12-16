4694115ae90bc4b1c2ba9aa4a7d6225dc08365cd
Trung Kien <21110223@student.hcmute.edu.vn>
2024-12-17 02:53:20 +0700

    deploy

--- views/src/utils/axiosInstance.ts
+++ views/src/utils/axiosInstance.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 import Cookie from "js-cookie";
 // import { BaseApi } from "./BaseApi";
-export const BaseApi = "http://localhost:8081";
+// export const BaseApi = "http://localhost:8081";
 // export const BaseApi = "https://destined-fly-rested.ngrok-free.app";
+export const BaseApi = "https://525b-2402-800-63b6-96c5-801f-41b5-9d40-7623.ngrok-free.app"
 export const axiosInstance = axios.create({
     baseURL: BaseApi,
     headers: {
