c182665e54a242ff49bbdbfcd4fc313eeae51dce
Trung Kien <21110223@student.hcmute.edu.vn>
2025-05-02 04:41:31 +0700

    Update axiosInstance.ts

--- views/src/utils/axiosInstance.ts
+++ views/src/utils/axiosInstance.ts
@@ -5,7 +5,7 @@ import Cookie from "js-cookie";
 // export const BaseApi = `http://localhost:${SERVER_PORT}`;
 // export const BaseApi = "https://0ac8-2402-800-6e0b-3ce1-761a-d053-e4a0-26a0.ngrok-free.app"
 // export const BaseApi = "https://destined-fly-rested.ngrok-free.app";
-export const BaseApi = "https://backend-production-df77.up.railway.app"
+export const BaseApi = "https://backend-2qyf.onrender.com"
 export const axiosInstance = axios.create({
     baseURL: BaseApi,
     headers: { 
