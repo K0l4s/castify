84b2be90f13009e33b25959fd86162f52590d55a
Trung Kien <21110223@student.hcmute.edu.vn>
2025-06-05 18:26:44 +0700

    Update DashboardService.ts

--- views/src/services/DashboardService.ts
+++ views/src/services/DashboardService.ts
@@ -6,6 +6,11 @@ export const DashboardService = {
             return axiosInstanceAuth.get(`/api/v1/admin/statistics`);
 
         return axiosInstanceAuth.get(`/api/v1/admin/statistics?startDate=${startDate}&endDate=${endDate}`);
+    },
+    getAdminGraphDashboard: async (startDate?: string, endDate?: string) => {
+        // return await axiosInstanceAuth.get('/api/v1/creator/statistics');
+        if(startDate === null || endDate === null || startDate === undefined || endDate === undefined)
+            return axiosInstanceAuth.get('/api/v1/admin/statistics/graph');
+        return axiosInstanceAuth.get(`/api/v1/admin/statistics/graph?startDate=${startDate}&endDate=${endDate}`);
     }
-
 };
