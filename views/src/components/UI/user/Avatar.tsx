71f869ce2fee7f7d8633dc151b62f757d679fd37
Huỳnh Trung Kiên <21110223@student.hcmute.edu.vn>
2025-06-12 05:40:20 +0700

    fix frame

--- views/src/components/UI/user/Avatar.tsx
+++ views/src/components/UI/user/Avatar.tsx
@@ -3,31 +3,31 @@ import defaultAvatar from "../../../assets/images/default_avatar.jpg"
 interface AvatarProps {
     avatarUrl?: string
     alt?: string
-    usedFrame?:UserFrame
+    usedFrame?: UserFrame
     userFrameUrl?: string
     width?: string
     height?: string
-    username?:string
+    username?: string
     onClick?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
-    onError?: (e:any) => void
+    onError?: (e: any) => void
 }
-const Avatar: React.FC<AvatarProps> = ({ avatarUrl,alt, width, height, usedFrame,userFrameUrl,onClick,onError}) => {
+const Avatar: React.FC<AvatarProps> = ({ avatarUrl, alt, width, height, usedFrame, userFrameUrl, onClick, onError }) => {
     return (
         <div className="relative" onClick={onClick}>
-    <img 
-        src={avatarUrl ? avatarUrl : defaultAvatar} 
-        alt={alt ? alt : "avatar"}
-        onError={onError}
-        className={`rounded-full ring-2 ring-gray-200 dark:ring-gray-700 ${width} ${height} object-cover`}
-    />
-    {usedFrame && (
-        <img 
-            src={usedFrame.imageURL} 
-            alt={usedFrame.name} 
-            className="absolute inset-0 object-contain scale-[1.2] pointer-events-none"
-        />
-    )}
-</div>
+            <img
+                src={avatarUrl ? avatarUrl : defaultAvatar}
+                alt={alt ? alt : "avatar"}
+                onError={onError}
+                className={`rounded-full ring-2 ring-gray-200 dark:ring-gray-700 ${width} ${height} object-cover`}
+            />
+            {usedFrame && (
+                <img
+                    src={usedFrame.imageURL}
+                    alt={usedFrame.name}
+                    className={`absolute inset-0 object-contain scale-[1.2] pointer-events-none ${width} ${height}`}
+                />
+            )}
+        </div>
 
     )
 }
